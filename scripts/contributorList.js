04bf58d17a1179f8a54999176d94cbcfac347ba3
swastiswagat <swagatswasti@gmail.com>
2025-06-04 11:40:48 +0530

    name added

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -362,4 +362,8 @@ let contributors = [
    "github": "https://github.com/Shreyanshsingh23",
   "name": "Shreyansh Singh Gautam "
   },
+      {
+        "github": "https://github.com/swastiswagat",
+        "name": "Swasti Swagat"
+    },
 ];
